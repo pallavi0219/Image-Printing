cf436545e029bab1a02ac8d53f75e7505c8ff86c
Bonthu Pallavi Durga <23nu5a4402@nsrit.edu.in>
2025-07-29 16:19:32 +0530

    Add files via upload
    Image printing produces high-quality physical copies of digital images. Using advanced printing technology, it delivers vibrant colors, precise details, and long-lasting results, ideal for photography, art, and personal projects, allowing users to tangibly showcase and share their favorite memories and creations.

---
+++ Image Printing.py
@@ -0,0 +1,4 @@
+import os
+
+os.startfile(r"C:\Users\ADMIN\Desktop\github projects pallavi\Image Printing\nature.jpg")
+os.startfile(r"C:\Users\ADMIN\Desktop\github projects pallavi\Image Printing\Flower.jpg")
